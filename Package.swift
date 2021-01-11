5ea0d099619614ed58f04d0419fd72ebd5ab7c12
moshekrush <moshe.krush@forter.com>
2021-01-11 23:18:22 +0200

    Test

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Forter3DS",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "ForterSDK",
+      targets: ["ForterSDK"]),
+    .library(
+      name: "ForterSDKStatic",
+      targets: ["ForterSDKStatic"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+  ],
+  targets: [
+    .binaryTarget(
+      name: "ForterSDK",
+      path: "SDK/xcframeworks/dynamic/Forter3DS.xcframework"
+    ),
+    .binaryTarget(
+      name: "ForterSDKStatic",
+      path: "SDK/xcframeworks/static/Forter3DS.xcframework"
+    )
+  ]
+)
